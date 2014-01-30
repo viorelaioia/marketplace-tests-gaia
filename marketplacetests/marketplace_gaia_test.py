bc608e0c271c5703cbf6b38d999a9c8ddfed4d14
Bob Silverberg <bob.silverberg@gmail.com>
2014-01-30 10:46:46 -0500

    Bug 965876 - Update marketplace-tests-gaia suite to install certs before test runs
    Add step to install certs and set pref needed for accessing marketplace-dev
    Add cert files to be pushed

--- marketplacetests/marketplace_gaia_test.py
+++ marketplacetests/marketplace_gaia_test.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
+
 from gaiatest.gaia_test import GaiaTestCase
 from marionette.by import By
 
@@ -10,8 +12,22 @@ class MarketplaceGaiaTestCase(GaiaTestCase):
 
     def setUp(self):
         GaiaTestCase.setUp(self)
+        self.install_certs()
         self.connect_to_network()
         self.install_marketplace()
+
+    def install_certs(self):
+        """ Install the marketplace-dev certs and set the pref required """
+        certs_folder = os.path.join('marketplacetests', 'certs')
+        for file_name in self.device.manager.listFiles('/data/b2g/mozilla/'):
+            if file_name.endswith('.default'):
+                profile_folder = file_name
+                break
+        for file_name in os.listdir(certs_folder):
+            self.device.push_file(os.path.join(certs_folder, file_name),
+                                  destination='data/b2g/mozilla/%s/%s' % (profile_folder, file_name))
+        self.data_layer.set_char_pref('dom.mozApps.signed_apps_installable_from',
+                                      'https://marketplace-dev.allizom.org,https://marketplace.firefox.com')
 
     def install_marketplace(self):
         _yes_button_locator = (By.ID, 'app-install-install-button')
