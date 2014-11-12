26151cc2a52627ead67a04b1aa5d1fa6d60fe96d
Viorela Ioia <viorelaioia@gmail.com>
2014-11-12 18:08:43 +0200

    Add sign in scenarios for FXA

--- marketplacetests/marketplace/regions/settings.py
+++ marketplacetests/marketplace/regions/settings.py
@@ -14,6 +14,8 @@ class Settings(Base):
     _sign_out_button_locator = (By.CSS_SELECTOR, 'a.button.logout')
     _back_button_locator = (By.ID, 'nav-back')
     _save_changes_button_locator = (By.XPATH, "//section[@id='account-settings']//button[text()='Save Changes']")
+    _my_apps_tab_locator = (By.CSS_SELECTOR, '.tab-link[href="/purchases"]')
+    _login_required_message_locator = (By.CSS_SELECTOR, '.only-logged-out .notice')
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
@@ -45,3 +47,25 @@ class Settings(Base):
     @property
     def email(self):
         return self.marionette.find_element(*self._email_account_field_locator).get_attribute('value')
+
+    def go_to_my_apps_page(self):
+        self.marionette.find_element(*self._my_apps_tab_locator).tap()
+        return MyApps(self.marionette)
+
+
+class MyApps(Base):
+
+    _login_required_message_locator = (By.CSS_SELECTOR, '.only-logged-out .notice')
+    _my_apps_list_locator = (By.CSS_SELECTOR, '.item.result')
+    _settings_page_locator = (By.CSS_SELECTOR, '.tab-link[href="/settings"]')
+
+    @property
+    def login_required_message(self):
+        return self.marionette.find_element(*self._login_required_message_locator).text
+
+    @property
+    def my_apps_list(self):
+        return self.marionette.find_elements(*self._my_apps_list_locator)
+
+    def go_to_settings_page(self):
+        self.marionette.find_element(*self._settings_page_locator).tap()
---
+++ marketplacetests/tests/test_marketplace_login_from_app_details_page.py
@@ -0,0 +1,43 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+import random
+
+from marketplacetests.marketplace_gaia_test import MarketplaceGaiaTestCase
+from marketplacetests.marketplace.app import Marketplace
+
+
+class TestMarketplaceLoginFromAppDetailsPage(MarketplaceGaiaTestCase):
+
+    def test_marketplace_login_from_app_details_page(self):
+        APP_NAME = 'SoundCloud'
+        username = self.testvars['marketplace']['username']
+        password = self.testvars['marketplace']['password']
+
+        marketplace = Marketplace(self.marionette, self.MARKETPLACE_DEV_NAME)
+        marketplace.launch()
+        details_page = marketplace.navigate_to_app(APP_NAME)
+
+        ff_accounts = details_page.tap_write_review(logged_in=False)
+        ff_accounts.login(username, password)
+
+        # switch back to Marketplace
+        marketplace.switch_to_marketplace_frame()
+        marketplace.wait_for_notification_message_displayed()
+        marketplace.wait_for_notification_message_not_displayed()
+
+        current_time = str(time.time()).split('.')[0]
+        rating = random.randint(1, 5)
+        body = 'This is a test %s' % current_time
+        from marketplacetests.marketplace.regions.review_box import AddReview
+        review_box = AddReview(self.marionette)
+
+        review_box.write_a_review(rating, body)
+        marketplace.wait_for_notification_message_displayed()
+
+        # Check if review was added correctly
+        self.assertEqual(marketplace.notification_message, "Your review was posted")
+        self.assertEqual(details_page.first_review_rating, rating)
+        self.assertEqual(details_page.first_review_body, body)
